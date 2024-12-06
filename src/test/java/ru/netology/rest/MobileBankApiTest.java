3d2bc764cd9614c598f90365b118a3a11a233396
ynappva@redmachinegames.com <cC255NQxG>
2024-12-06 19:47:08 +0300

    test added

--- src/test/java/ru/netology/rest/MobileBankApiTest.java
+++ src/test/java/ru/netology/rest/MobileBankApiTest.java
@@ -2,6 +2,23 @@ package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTest {
+
+    @Test
+
+            void shouldReturnPostedBody() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("som data"));
+    }
+
 }
